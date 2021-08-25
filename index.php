a8280c57447d128e4ece9f979144536e43a37192
Nannyju <88876387+Nannyju@users.noreply.github.com>
2021-08-25 22:54:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <!doctype html>
 <html lang="en">
   <head>
-  
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello, world!</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-
-    
+    <title>Web Graph</title>
     <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.0/dist/chart.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.0/dist/chart.min.js"></script>
   </head>
 
   <body>
+
+    <h1>62101639 Julaluck Noosuwan</h1>
 
     <div class="container">
       <div class="row">
@@ -57,119 +55,99 @@
     </div>
 
 
-    
-
-    
-   
-
   </body>
 
 
-    <script>
+  <script>
 
-function showLine(chartid,data){
-        var ctx = document.getElementById(chartid).getContext('2d');
-        var mychart = new Chart(ctx, {
-          type: 'line',
-          data: {
-            
-              labels: data.xlabel,
-              datasets: [
-                {
-                  label: data.label,
-                  data: data.data
-                }
-              ]
+    function showChart(data){
+      var ctx=document.getElementById('myChart').getContext('2d');
+      var myChart=new Chart(ctx,{
+          type:'line',
+          data:{
+            labels:data.xlabel,
+            datasets:[{
+                label:data.label,
+                data:data.data
+            }]
           }
+      });
+    }
 
-        });
-      }
-
-
-
-
-
-
-
-
-
-
-      function showChart(data){
-        var ctx = document.getElementById('myChart').getContext('2d');
-
-        var xlabel = [1,2,3,4,5,6,7,8,9,10];
-        var data1 = [2,3,4,5,10];
-        var data2 = [2,4,5,6,10];
-
-        var mychart = new Chart(ctx, {
-          type: 'line',
-          data: {
-            
-              labels: xlabel,
-              datasets: [
-                {
-                  label: "1st line",
-                  data: data1
-                },
-
-                {
-                  label: "2nd line",
-                  data: data2
-                }
-              ]
+    function showLine(data2){
+      var ctxy=document.getElementById('myChart2').getContext('2d');
+      var myChart=new Chart(ctxy,{
+          type:'line',
+          data:{
+            labels:data2.xlabel,
+            datasets:[{
+                label:data2.label,
+                data:data2.data
+            }]
           }
+      });
+    }
+      
+      
+     
+      $(()=>{
+        //alert("Hello");
+          
+          let url="https://api.thingspeak.com/channels/1479261/feeds.json?results=50";
+          $.getJSON(url)
+            .done(function(data){
+              //console.log(data);
+              let feed=data.feeds;
+              let chan=data.channel;
+              console.log(feed);
+              //let chan = data.channel;
 
-        });
-      }
+
+              const d =new Date(feed[0].created_at);
+              const monthNames=["January","February","March","April","May","June","July"
+                                ,"August","September","October","November","December"];
+              let dateStr = d.getDate()+" "+monthNames[d.getMonth()]+" "+d.getFullYear();
+              dateStr += " "+d.getHours()+":"+d.getMinutes();
 
 
+             var plot_data=Object();
+             var xlabel=[];
+             var tempe=[];
+             var humidi=[];
+
+             
+             $.each(feed,(k,v)=>{
+                xlabel.push(v.entry_id);
+                humidi.push(v.field1);
+                tempe.push(v.field2);
+                console.log(k,humidi);
+             });
+
+             
+              var data=new Object();
+              data.xlabel=xlabel;
+              data.data=tempe;
+              data.label=chan.field2;
+              showChart(data);
+
+            
+              var data2=new Object();
+              data2.xlabel=xlabel;
+              data2.data=humidi;
+              data2.label=chan.field1;
+              showLine(data2);
+
+
+              $("#lastTemperature").text(feed[0].field2+" C");
+              $("#lastHumidity").text(feed[0].field1+" %");
+              $("#lastUpdate").text(dateStr);
+              console.log(humidi);
+              
+            })
+            .fail(function(error){
+               //console.log(error);
+            });
+      });
     
-       $(()=>{
-           //alert("Hello");
-          let url = "https://api.thingspeak.com/channels/1479261/feeds.json?results=1";
-           $.getJSON(url)
-           .done(function(data){
-            console.log(data);
-             //console.log(data.channel.name);
-
-             let feeds = data.feeds;
-             console.log(feeds);
-
-
-             $("#lastTemperature").text(feeds[0].field2 + "c");
-             $("#lastHumidity").text(feeds[0].field1 + "%");
-             $("#lastUpdate").text(feeds[0].created_at);
-
-
-
-             //showChart(data);
-
-             var data = new Object();
-             data.label = "1st line";
-             data.xlabel = [1,2,3,4,5,6,7,8,9,10];
-             data.data = [3,8,2,4,1];
-             showLine("myChart",data);
-
-
-
-             var data2 = new Object();
-             data2.label = "2nd line";
-             data2.xlabel = [11,12,13,14,15];
-             data2.data = [30,18,20,14,10];
-             showLine("myChart2",data2);
-
-
-           })
-           
-           .fail(function(error){
-             console.log(error);
-
-           });
-
-
-       });
-
-       
-
-    </script>
+  </script>
 </html>
